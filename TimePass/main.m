a400f5ebfc25ebdf595de2551fb6ba38d984ddd5
Giorgio Iavicoli <giorgio.iavicoli@gmail.com>
2018-08-29 22:42:10 +0200

    Initial Commit

---
+++ TimePass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimePass
+//
+//  Created by Giorgio Iavicoli on 29/08/2018.
+//  Copyright © 2018 Giorgio Iavicoli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
